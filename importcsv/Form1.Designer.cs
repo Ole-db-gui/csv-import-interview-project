845cf230cb95787e4dfb31239a394a55c48d746b
Ole-db-gui <pranayamasimhamudra@gmail.com>
2019-06-27 23:15:54 +0500

    Initial commit

---
+++ importcsv/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace importcsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnopen = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnopen
+            // 
+            this.btnopen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnopen.Location = new System.Drawing.Point(1126, 439);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(94, 23);
+            this.btnopen.TabIndex = 0;
+            this.btnopen.Text = "Open CSV";
+            this.btnopen.UseVisualStyleBackColor = true;
+            this.btnopen.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImport.Location = new System.Drawing.Point(1126, 481);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(94, 23);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import to SQL";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.BtnImport_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(1126, 517);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileName.Location = new System.Drawing.Point(24, 440);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(1093, 20);
+            this.txtFileName.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1211, 305);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(557, 52);
+            this.label1.TabIndex = 5;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.PrintDocument1_PrintPage);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1126, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Print current view";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 568);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnopen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnopen;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ importcsv/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace importcsv
+{
+    public partial class Form1 : Form
+    {
+
+        string ConnStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\HQ\APPDATA\LOCAL\MICROSOFT\MICROSOFT SQL SERVER LOCAL DB\INSTANCES\MSSQLLOCALDB\CSV DATA.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static string activeTable;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = ".csv";
+            ofd.Filter = "CSV files (*.csv)|*.csv";
+            ofd.ShowDialog();
+
+            txtFileName.Text = ofd.FileName;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        String generateRandomString(int length)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var stringChars = new char[length];
+            var random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new String(stringChars);
+        }
+
+        String createTable(string[] headers, SqlConnection conn)
+        {
+            List<string> columns = new List<string>();
+            foreach (string header in headers)
+            {
+                columns.Add(string.Format("{0} NVARCHAR(MAX)", header));
+            }
+
+            List<string> goodNames = new List<String>();
+
+
+
+            String tableName = generateRandomString(10);
+            activeTable = tableName;
+            String query = string.Format("CREATE TABLE {0}({1});", tableName, string.Join(",", columns));
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            return tableName;
+        }
+
+        void insertColumns(string tableName, string[] headers, string[] columns, SqlConnection connection)
+        {
+            if (headers.Length != columns.Length)
+            {
+                throw new System.ArgumentException("Length of headers must be the same as columns length");
+            }
+
+            List<string> headersWithAtSign = new List<string>(headers).Select(header => "@" + header).ToList();
+
+            String query = string.Format("INSERT INTO {0} ({1}) VALUES ({2});", tableName, string.Join(",", headers), string.Join(",", headersWithAtSign));
+            SqlCommand cmd = new SqlCommand(query, connection);
+            for (int i = 0; i < columns.Length; i++)
+            {
+                cmd.Parameters.AddWithValue("@" + headers[i], columns[i]);
+            }
+
+            cmd.ExecuteNonQuery();
+
+
+        }
+
+        void ReadAndImport()
+        {
+            using (TextFieldParser tfp = new TextFieldParser(txtFileName.Text))
+            {
+                if (tfp.EndOfData)
+                {
+                    Console.WriteLine("File is empty");
+                    return;
+                }
+
+                using (SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\HQ\APPDATA\LOCAL\MICROSOFT\MICROSOFT SQL SERVER LOCAL DB\INSTANCES\MSSQLLOCALDB\CSV DATA.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+                {
+                    conn.Open();
+
+                    //SqlCommand cmd = new SqlCommand("CREATE TABLE test(First_Name NVARCHAR(MAX), Birth_Date NVARCHAR(MAX));", conn);
+                    //cmd.ExecuteNonQuery();
+                    tfp.TextFieldType = FieldType.Delimited;
+                    tfp.SetDelimiters(";");
+                    List<string[]> temp = new List<string[]>();
+
+                    // NEW
+                    int headersCount = tfp.ReadFields().Length;
+                    string[] headers = Enumerable.Range(0, headersCount).Select(i => "column_" + i).ToArray();
+                    string tableName = createTable(headers, conn);
+                    //
+
+                    while (!tfp.EndOfData)
+                    {
+                        string[] fields = tfp.ReadFields();
+                        temp.Add(fields);
+                        insertColumns(tableName, headers, fields, conn); // NEW
+
+                    }
+                }
+            }
+        }
+        private void BtnImport_Click(object sender, EventArgs e)
+        {
+            ReadAndImport();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillTable();
+        }
+
+        // Показать таблицу
+        private void FillTable()
+        {
+            string SqlText = string.Format("SELECT * FROM {0};", activeTable);
+            SqlDataAdapter da = new SqlDataAdapter(SqlText, ConnStr);
+            DataSet ds = new DataSet();
+            da.Fill(ds, activeTable);
+            dataGridView1.DataSource = ds.Tables[activeTable].DefaultView;
+        }
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            FillTable();
+        }
+
+        private void PrintDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(dataGridView1.Size.Width+10, dataGridView1.Size.Height+10);
+            dataGridView1.DrawToBitmap(bmp, dataGridView1.Bounds);
+            e.Graphics.DrawImage(bmp, 0, 0);
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            printDocument1.DefaultPageSettings.Landscape = true;
+            printDocument1.Print();
+        }
+    }
+}
